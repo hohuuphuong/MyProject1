c194b9ee7b7d12246af86c0434f9161fa7ef4328
hohuuphuong <hohuuphuong2804@gmail.com>
2023-11-04 23:04:18 +0700

    test

--- NewsApp1/app/src/main/java/com/example/myapplication/ui/screens/DetailScreen.kt
+++ NewsApp1/app/src/main/java/com/example/myapplication/ui/screens/DetailScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
@@ -102,4 +103,6 @@ fun DetailContent(
         }
     }
 }
+
+
 
--- WebAPI/nhom 13/Migrations/20231104155818_init.Designer.cs
+++ WebAPI/nhom 13/Migrations/20231104155818_init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using nhom_13.data;
 
@@ -11,9 +12,11 @@ using nhom_13.data;
 namespace nhom_13.Migrations
 {
     [DbContext(typeof(MyDBContext))]
-    partial class MyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231104155818_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
